0d0212921e9c3d3bbf1db9249e7b2276f57bb910
HotDucky46 <122406210+teenage-technology@users.noreply.github.com>
2023-05-19 08:34:27 -0500

    header.js

---
+++ header.js
@@ -0,0 +1,11 @@
+// header.js
+document.addEventListener("DOMContentLoaded", function () {
+    var headerContainer = document.getElementById("header-container");
+    fetch("header.html")
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (html) {
+            headerContainer.innerHTML = html;
+        });
+});
