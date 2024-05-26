aebabea31bfca45da0a163328a9ce9d33b073690
Owen Van Vooren <ovsquared@icloud.com>
2024-05-26 16:07:28 -0500

    AutoUpdate
    This code update is automatically and autonomously generated and distributed.

---
+++ firebaseconfig.js
@@ -0,0 +1,15 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyCAAIxwAqHedBT3c1fuTUkaDBxQ0MaKsRE",
+    authDomain: "teenage-tech.firebaseapp.com",
+    projectId: "teenage-tech",
+    storageBucket: "teenage-tech.appspot.com",
+    messagingSenderId: "8796698339",
+    appId: "1:8796698339:web:ab9388605c5d7d6c0577d6",
+    measurementId: "G-N7Q05TGCJ6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
